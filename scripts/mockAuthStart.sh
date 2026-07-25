7252716a6fc1e5f5466ac83bf4c3a67a7749e6c9
Thijmen-Jansen <t.jansen24@student.han.nl>
2026-07-25 15:53:22 +0200

    setup locally

--- scripts/mockAuthStart.sh
+++ scripts/mockAuthStart.sh
@@ -3,6 +3,6 @@ set -e
 
 # start container if it exists, otherwise create and run
 docker start mock-auth 2>/dev/null || \
-  docker run --name mock-auth -d \
+  MSYS_NO_PATHCONV=1 docker run --name mock-auth -d \
   -p 9000:9000 \
   mock-auth
